af2fa2296138dd16ac52c077ad6c1c19002fc251
imubarak234 <imubarak2424@gmail.com>
2025-06-16 21:13:00 +0100

    background images for game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,43 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View, Image, Text } from "react-native";
 import Title from "../components/ui/Title";
+import Colors from "../constants/Color";
 
 const GameOverScreen = () => {
 
-  return <View>
-    <Title>GAME OVER!</Title>
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image 
+          source={require('../assets/images/success.png')} 
+          style={styles.image}
+        />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y.</Text>
   </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  }
+})
 
 export default GameOverScreen;
